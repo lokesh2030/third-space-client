a1fc5bc765a65b4c23b620226cd36ae227870f21
lokesh2030 <lokeshster@gmail.com>
2025-04-21 21:56:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const BACKEND_URL = "https://third-space-backend.onrender.com";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function App() {
   const [input, setInput] = useState("");
