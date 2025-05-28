f65242cfa937f71895505c3b9f36cc67005f7aeb
lokesh2030 <lokeshster@gmail.com>
2025-05-27 19:53:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -149,7 +149,7 @@ export default function App() {
         } catch (err) {
           setOutput("Error: " + err.message);
         }
-      }, 86,400,000);
+      }, 86400000);
       return () => clearInterval(interval);
     }
   }, [mode]);
