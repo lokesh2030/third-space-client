44025bb0bffdc5db9fd0e566122e274c4b3410ca
lokesh2030 <lokeshster@gmail.com>
2025-06-04 13:59:57 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -167,7 +167,7 @@ export default function App() {
             }
 
             currentIndex++;
-          }, 30000);
+          }, 86400000);
         });
 
       return () => clearInterval(intervalId);
